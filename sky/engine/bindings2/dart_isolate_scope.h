46c5e98f4acbda15c6988ea6a54d49cd66986aa3
Adam Barth <abarth@chromium.org>
2015-02-02 10:35:25 -0800

    Add DartIsolateScope
    TBR=rafaelw@chromium.org
    
    Review URL: https://codereview.chromium.org/894113002

---
+++ sky/engine/bindings2/dart_isolate_scope.h
@@ -0,0 +1,26 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKY_ENGINE_BINDINGS2_DART_ISOLATE_SCOPE_H_
+#define SKY_ENGINE_BINDINGS2_DART_ISOLATE_SCOPE_H_
+
+#include "base/logging.h"
+#include "dart/runtime/include/dart_api.h"
+#include "sky/engine/wtf/Noncopyable.h"
+
+namespace blink {
+
+class DartIsolateScope {
+ public:
+  explicit DartIsolateScope(Dart_Isolate isolate);
+  ~DartIsolateScope();
+
+ private:
+  Dart_Isolate isolate_;
+  Dart_Isolate previous_;
+};
+
+}
+
+#endif // SKY_ENGINE_BINDINGS2_DART_ISOLATE_SCOPE_H_
